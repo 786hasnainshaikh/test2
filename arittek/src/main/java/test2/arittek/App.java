ad98a16fabaecc466641f36e5eccb9def6b58f95
MuhammadHusnain <muhammad.husnain@paysyslabs.com>
2025-11-12 13:40:55 +0500

    kdjfkfgkf

---
+++ arittek/src/main/java/test2/arittek/App.java
@@ -0,0 +1,13 @@
+package test2.arittek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
